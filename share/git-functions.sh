608b026501380d4cbd21a6f804ee048685a6aaae
Michal Maruska <mmaruska@gmail.com>
2016-12-16 13:07:10 +0100

    set_remote_name: use in git-add-remote-mdell, git-pull-from-remote: -r
    Also use the system tools.
    
    Change-Id: Id7b3c7dd6ec342497ff989e4cc8776a3b2a5fa16

--- share/git-functions.sh
+++ share/git-functions.sh
@@ -1,6 +1,18 @@
 #!/bin/zsh -f
 # to be sourced!
 
+# sets GIT_REMOTE_NAME and GIT_URL
+set_remote_name()
+{
+    if [ $(hostname) = e6440 ]; then
+        : ${GIT_REMOTE_NAME:=optiplex}
+        : ${GIT_URL:=michal@optiplex}
+    else
+        : ${GIT_REMOTE_NAME=e6440}
+        : ${GIT_URL=michal@e6440}
+    fi
+}
+
 function get_local_path()
 {
     local TOP=$1
