52ca46410c315113252d2e0e0a1729b84241b863
Michal Maruska <mmaruska@gmail.com>
2018-12-27 11:16:03 +0100

    git-ff: default to the "friendly remote" and the current branch
    Change-Id: I0601677d99226e4c51d161aefe1307aefefbdd02

--- share/git-functions.sh
+++ share/git-functions.sh
@@ -7,7 +7,7 @@ set_remote_name()
     if [ $(hostname) = e6440 ]; then
         : ${GIT_REMOTE_NAME:=optiplex}
         : ${GIT_URL:=michal@optiplex}
-    else
+    else # [ $(hostname) = "optiplex-maruska" ];
         : ${GIT_REMOTE_NAME=e6440}
         : ${GIT_URL=michal@e6440}
     fi
@@ -24,4 +24,19 @@ function get_local_path()
 function git_ref_exists()
 {
     [ -e .git/refs/$1 ]
+}
+
+# please use the one in ~/repo/git-hierarchy/share/functions.sh
+# return the branch checked-out. Error if in "detached HEAD" state.
+function current_branch_name()
+{
+    local head
+    head=$(git rev-parse --symbolic-full-name HEAD)
+    head=${head##refs/heads/}
+    if [ $head = HEAD ]; then
+        cecho red "currently not on a branch" >&2
+        exit 1;
+    fi
+
+    echo "$head"
 }
